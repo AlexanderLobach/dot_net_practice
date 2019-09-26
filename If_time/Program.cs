8b7c5962340411881ef40bf61abb4c9be495a056
AlexanderLobach <375296255246@ya.ru>
2019-09-27 00:48:55 +0300

    If_Time

---
+++ If_time/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace If_time
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           TimeSpan current_time = DateTime.Now.TimeOfDay;
+
+Console.WriteLine( current_time);
+
+int current_hour = current_time.Hours;
+
+if (current_hour>=9 & current_hour<12)
+{
+    Console.WriteLine("Good morning, guys!");
+}
+
+
+if (current_hour>=12 & current_hour<15)
+{
+    Console.WriteLine("Good day, guys!");
+}
+
+
+if (current_hour>=15 & current_hour<22)
+{
+    Console.WriteLine("Good evening, guys!");
+}
+
+
+if (current_hour>=22 || current_hour<9)
+{
+    Console.WriteLine("Good night, guys!");
+}   
+
+Console.ReadLine();
+
+
+        }
+    }
+}
+
