f047e41111dd4ec94901f5e67eaffc6b7857c6a6
AlexanderLobach <375296255246@ya.ru>
2019-09-27 17:42:54 +0300

    add cycle while for time

--- If_time/Program.cs
+++ If_time/Program.cs
@@ -6,38 +6,69 @@ namespace If_time
     {
         static void Main(string[] args)
         {
-           TimeSpan current_time = DateTime.Now.TimeOfDay;
-
-Console.WriteLine( current_time);
+          Console.WriteLine("Enter your name:");
+          string Name= Console.ReadLine();     
+          Console.Clear();
+          //var btn= ConsoleKey.Escape;
+          ConsoleKeyInfo cki= new ConsoleKeyInfo();
+          
+       while (true)
+       {
+           
+      Console.Clear();
+                         
+          TimeSpan current_time = DateTime.Now.TimeOfDay;
+          Console.WriteLine($"current time {current_time.ToString(@"hh\:mm\:ss")}");
+        
 
 int current_hour = current_time.Hours;
 
 if (current_hour>=9 & current_hour<12)
 {
-    Console.WriteLine("Good morning, guys!");
+    Console.WriteLine($"Good morning, {Name}!");
 }
 
 
 if (current_hour>=12 & current_hour<15)
 {
-    Console.WriteLine("Good day, guys!");
+    Console.WriteLine($"Good day, {Name}!");
 }
 
 
 if (current_hour>=15 & current_hour<22)
 {
-    Console.WriteLine("Good evening, guys!");
+    Console.WriteLine($"Good evening, {Name}!");
 }
 
 
 if (current_hour>=22 || current_hour<9)
 {
-    Console.WriteLine("Good night, guys!");
+    Console.WriteLine($"Good night, {Name}!");
 }   
 
-Console.ReadLine();
+Console.WriteLine("\nPress ESC to exit");
+System.Threading.Thread.Sleep(1000);
+    
 
+if (Console.KeyAvailable == true)
 
+                {
+
+                    cki = Console.ReadKey(true);
+
+                    if (cki.Key == ConsoleKey.Escape)
+
+                    {
+
+                        break;
+
+                    }
+
+                }
+
+            }
         }
     }
 }
+
+ 
