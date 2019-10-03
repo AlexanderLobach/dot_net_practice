2aed3d452c0fe0e94c06575ff89489102a99e489
AlexanderLobach <375296255246@ya.ru>
2019-10-03 19:35:17 +0300

    task1

---
+++ P_06_Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace P_06_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void emptyArray ();
+        }
+        
+        static void emptyArray ()
+        {
+            int[] nums = new int[4];
+        }
+
+
+    }
+}
