e3704c563dac8680d836c0c8fb423ee79f06cfa2
AlexanderLobach <375296255246@ya.ru>
2019-10-03 20:05:45 +0300

    task2

---
+++ P_06_Task2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace P_06_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            sArray ();
+        }
+
+         static void sArray ()
+        {
+            string [] nums = new string[3];
+            nums[0]="32";
+            nums[1]="A";
+            nums[2]= "Hello";
+            for (int i = 0; i <3; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+            nums[0] = nums[0] + 32;
+            nums[2] = nums[2] + ", gyus!";
+             for (int i = 0; i <3; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+        }
+
+    }
+}
