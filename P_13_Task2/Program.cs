73746f125e76c68c08cff94361898982f726d2c9
AlexanderLobach <375296255246@ya.ru>
2019-11-05 14:23:29 +0300

    Add P_13_Task2

---
+++ P_13_Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P_13_Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Какой хороший день";
+            string textCut = CutEllipsis(text);
+            Console.WriteLine(textCut);
+        }
+            static string CutEllipsis(string text)
+            {
+                if (text.Length > 5)
+                {
+                    text = text.Substring(0, 5) + "...";
+                }
+                return text;
+            }    
+    }
+}
